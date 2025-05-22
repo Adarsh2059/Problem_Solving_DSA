ec4c1c4f75c73748fe9999e34c0e30a61cab9e2f
Adarsh Yadav <adarsh.yadav070605@gmail.com>
2025-05-22 13:30:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==')' && count<=0){s[i]='*';}
+            if(s[i]==')' && count>0){count--;}
+            if(s[i]=='('){count++;}
+        }
+        count=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='(' && count>0){s[i]='*';count--;}
+            if(s[i]=='(' && count<=0){count++;}
+            if(s[i]==')'){count=0;}
+        }
+        int start=0;
+        int end=0;
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='*'){
+                ans=max(ans,(end-start));
+                start=i+1;
+                end=i+1;
+            }
+            else{
+                end=i+1;
+            }
+        }
+        ans=max(ans,(end-start));
+        return ans;
+    }
+};
