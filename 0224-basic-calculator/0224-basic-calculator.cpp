7135c49546237a74906ec9a902559511d3c142fe
Adarsh Yadav <adarsh.yadav070605@gmail.com>
2025-05-29 18:15:35 +0530

    Time: 4 ms (30.23%), Space: 10.5 MB (76.26%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int> stk;
-        int result = 0;
-        int sign = 1;  
-        int number = 0;
-
-        for (int i = 0; i < s.length(); ++i) {
-            char ch = s[i];
-            if (isdigit(ch)) {
-                number = 0;
-                while (i < s.length() && isdigit(s[i])) {
-                    number = number * 10 + (s[i] - '0');
-                    i++;
-                }
-                result += sign * number;
-                i--; 
-            } 
-            else if (ch == '+') {
-                sign = 1;
-            } 
-            else if (ch == '-') {
-                sign = -1;
-            } 
-            else if (ch == '(') {
-                stk.push(result);
-                stk.push(sign);
-                result = 0;
-                sign = 1;
-            } 
-            else if (ch == ')') {
-                int prevSign = stk.top(); stk.pop();
-                int prevResult = stk.top(); stk.pop();
-                result = prevResult + prevSign * result;
+        stack<int> st;
+        int result=0;
+        int sign=1;
+        int number=0;
+        for(char ch:s){
+            if(isdigit(ch)){
+                number=number*10+(ch-'0');
+            }
+            else if(ch=='+'){
+                result+=sign*number;
+                number=0;
+                sign=1;
+            }
+            else if(ch=='-'){
+                result+=sign*number;
+                number=0;
+                sign=-1;
+            }
+            if(ch=='('){
+                st.push(result);
+                st.push(sign);
+                result=0;
+                sign=1;
+            }
+            if(ch==')'){
+                result+=sign*number;
+                number=0;
+                int p_sign=st.top();st.pop();
+                int p_result=st.top();st.pop();
+                result=p_result+p_sign*result;
             }
         }
+        result+=sign*number;
         return result;
     }
 };
