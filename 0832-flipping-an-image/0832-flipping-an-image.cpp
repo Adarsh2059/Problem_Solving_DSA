fd867fef901f0b259d7bd4941ad6d18c20d0e9ce
Adarsh Yadav <adarsh.yadav070605@gmail.com>
2025-06-04 20:46:04 +0530

    Time: 0 ms (100%), Space: 12.4 MB (99.4%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.cpp
+++ 0832-flipping-an-image/0832-flipping-an-image.cpp
@@ -4,8 +4,6 @@ public:
         for (auto& row : image) {
             int n = row.size();
             for (int i = 0; i < (n + 1) / 2; ++i) {
-                // Swap and invert in one step
-                // XOR with 1 flips 0 -> 1 and 1 -> 0
                 int temp = row[i] ^ 1;
                 row[i] = row[n - 1 - i] ^ 1;
                 row[n - 1 - i] = temp;
