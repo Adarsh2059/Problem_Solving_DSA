70b277657ce2cd9c4ac1ac28cc143846e7c94662
Adarsh Yadav <adarsh.yadav070605@gmail.com>
2025-06-04 20:07:35 +0530

    Time: 11 ms (53.15%), Space: 13.3 MB (23.87%) - LeetHub

--- 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.cpp
+++ 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
-    // Function to compute the Least Common Multiple (LCM) of two numbers
-    int lcm(int a, int b) {
-        return (a * b) / gcd(a, b); // LCM(a, b) = (a * b) / GCD(a, b)
-    }
-
     int subarrayLCM(vector<int>& nums, int k) {
-        int count = 0;
-        int n = nums.size();
-        
-        // Iterate over each possible subarray
-        for (int i = 0; i < n; i++) {
-            int lcm_val = nums[i]; // Start with the current element
-            
-            // For each subarray starting from index i
-            for (int j = i; j < n; j++) {
-                // Update LCM of the current subarray
-                lcm_val = lcm(lcm_val, nums[j]);
-                
-                // If LCM exceeds k, break out of the inner loop
-                if (lcm_val > k) {
-                    break;
-                }
-                
-                // If LCM equals k, increment the count
-                if (lcm_val == k) {
-                    count++;
-                }
+        int n = nums.size(), count = 0;
+        for (int i = 0; i < n; ++i) {
+            int lcm = nums[i];
+            for (int j = i; j < n; ++j) {
+                lcm = lcm / gcd(lcm, nums[j]) * nums[j];
+                if (lcm == k) count++;
+                if (lcm > k) break;
             }
         }
         return count;
+    }
+private:
+    int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 };
